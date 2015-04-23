64be295d148d35344fd368f7837ba052ff091294
warelle <fsfresh@gmail.com>
2015-04-23 11:40:59 +0900

    ld: add reloc functions

--- bfd/elf32-aqua.c
+++ bfd/elf32-aqua.c
@@ -178,6 +178,228 @@ aqua_elf_info_to_howto (bfd *abfd ATTRIBUTE_UNUSED,
   cache_ptr->howto = &aqua_elf_howto_table[r];
 }
 
+/* Perform a single relocation.  By default we use the standard BFD
+   routines, but a few relocs, we have to do them ourselves.  */
+static bfd_reloc_status_type
+aqua_final_link_relocate (reloc_howto_type *howto,
+			   bfd *input_bfd,
+			   asection *input_section,
+			   bfd_byte *contents,
+			   Elf_Internal_Rela *rel,
+			   bfd_vma relocation)
+{
+  bfd_reloc_status_type r = bfd_reloc_ok;
+
+  switch (howto->type)
+  {
+    default:
+      r = _bfd_final_link_relocate (howto, input_bfd, input_section,
+          contents, rel->r_offset,
+          relocation, rel->r_addend);
+  }
+
+  return r;
+}
+
+/* Relocate an AQUA ELF section.
+
+   The RELOCATE_SECTION function is called by the new ELF backend linker
+   to handle the relocations for a section.
+
+   The relocs are always passed as Rela structures; if the section
+   actually uses Rel structures, the r_addend field will always be
+   zero.
+
+   This function is responsible for adjusting the section contents as
+   necessary, and (if using Rela relocs and generating a relocatable
+   output file) adjusting the reloc addend as necessary.
+
+   This function does not have to worry about setting the reloc
+   address or the reloc symbol index.
+
+   LOCAL_SYMS is a pointer to the swapped in local symbols.
+
+   LOCAL_SECTIONS is an array giving the section in the input file
+   corresponding to the st_shndx field of each local symbol.
+
+   The global hash table entry for the global symbols can be found
+   via elf_sym_hashes (input_bfd).
+
+   When generating relocatable output, this function must handle
+   STB_LOCAL/STT_SECTION symbols specially.  The output symbol is
+   going to be the section symbol corresponding to the output
+   section, which means that the addend must be adjusted
+   accordingly.  */
+static bfd_boolean
+aqua_elf_relocate_section (bfd *output_bfd,
+			    struct bfd_link_info *info,
+			    bfd *input_bfd,
+			    asection *input_section,
+			    bfd_byte *contents,
+			    Elf_Internal_Rela *relocs,
+			    Elf_Internal_Sym *local_syms,
+			    asection **local_sections)
+{
+  Elf_Internal_Shdr *symtab_hdr;
+  struct elf_link_hash_entry **sym_hashes;
+  Elf_Internal_Rela *rel;
+  Elf_Internal_Rela *relend;
+
+  symtab_hdr = & elf_tdata (input_bfd)->symtab_hdr;
+  sym_hashes = elf_sym_hashes (input_bfd);
+  relend     = relocs + input_section->reloc_count;
+
+  for (rel = relocs; rel < relend; rel ++)
+  {
+    reloc_howto_type *howto;
+    unsigned long r_symndx;
+    Elf_Internal_Sym *sym;
+    asection *sec;
+    struct elf_link_hash_entry *h;
+    bfd_vma relocation;
+    bfd_reloc_status_type r;
+    const char *name;
+    int r_type;
+
+    r_type = ELF32_R_TYPE (rel->r_info);
+    r_symndx = ELF32_R_SYM (rel->r_info);
+    howto  = aqua_elf_howto_table + r_type;
+    h      = NULL;
+    sym    = NULL;
+    sec    = NULL;
+
+    if (r_symndx < symtab_hdr->sh_info)
+    {
+      sym = local_syms + r_symndx;
+      sec = local_sections [r_symndx];
+      relocation = _bfd_elf_rela_local_sym (output_bfd, sym, &sec, rel);
+
+      name = bfd_elf_string_from_elf_section
+        (input_bfd, symtab_hdr->sh_link, sym->st_name);
+      name = (name == NULL) ? bfd_section_name (input_bfd, sec) : name;
+    }
+    else
+    {
+      bfd_boolean unresolved_reloc, warned, ignored;
+
+      RELOC_FOR_GLOBAL_SYMBOL (info, input_bfd, input_section, rel,
+          r_symndx, symtab_hdr, sym_hashes,
+          h, sec, relocation,
+          unresolved_reloc, warned, ignored);
+
+      name = h->root.root.string;
+    }
+
+    if (sec != NULL && discarded_section (sec))
+      RELOC_AGAINST_DISCARDED_SECTION (info, input_bfd, input_section,
+          rel, 1, relend, howto, 0, contents);
+
+    if (info->relocatable)
+      continue;
+
+    r = aqua_final_link_relocate (howto, input_bfd, input_section,
+        contents, rel, relocation);
+
+    if (r != bfd_reloc_ok)
+    {
+      const char * msg = NULL;
+
+      switch (r)
+      {
+        case bfd_reloc_overflow:
+          r = info->callbacks->reloc_overflow
+            (info, (h ? &h->root : NULL), name, howto->name,
+             (bfd_vma) 0, input_bfd, input_section, rel->r_offset);
+          break;
+
+        case bfd_reloc_undefined:
+          r = info->callbacks->undefined_symbol
+            (info, name, input_bfd, input_section, rel->r_offset,
+             TRUE);
+          break;
+
+        case bfd_reloc_outofrange:
+          msg = _("internal error: out of range error");
+          break;
+
+        case bfd_reloc_notsupported:
+          msg = _("internal error: unsupported relocation error");
+          break;
+
+        case bfd_reloc_dangerous:
+          msg = _("internal error: dangerous relocation");
+          break;
+
+        default:
+          msg = _("internal error: unknown error");
+          break;
+      }
+
+      if (msg)
+        r = info->callbacks->warning
+          (info, msg, name, input_bfd, input_section, rel->r_offset);
+
+      if (! r)
+        return FALSE;
+    }
+  }
+
+  return TRUE;
+}
+
+/* Return the section that should be marked against GC for a given relocation.  */
+static asection *
+aqua_elf_gc_mark_hook (asection *sec,
+			struct bfd_link_info *info,
+			Elf_Internal_Rela *rel,
+			struct elf_link_hash_entry *h,
+			Elf_Internal_Sym *sym)
+{
+  return _bfd_elf_gc_mark_hook (sec, info, rel, h, sym);
+}
+
+static bfd_boolean
+aqua_elf_check_relocs (bfd *abfd,
+			struct bfd_link_info *info,
+			asection *sec,
+			const Elf_Internal_Rela *relocs)
+{
+  Elf_Internal_Shdr *symtab_hdr;
+  struct elf_link_hash_entry **sym_hashes;
+  const Elf_Internal_Rela *rel;
+  const Elf_Internal_Rela *rel_end;
+
+  if (info->relocatable)
+    return TRUE;
+
+  symtab_hdr = &elf_tdata (abfd)->symtab_hdr;
+  sym_hashes = elf_sym_hashes (abfd);
+
+  rel_end = relocs + sec->reloc_count;
+  for (rel = relocs; rel < rel_end; rel++)
+  {
+    struct elf_link_hash_entry *h;
+    unsigned long r_symndx;
+
+    r_symndx = ELF32_R_SYM (rel->r_info);
+    if (r_symndx < symtab_hdr->sh_info)
+      h = NULL;
+    else
+    {
+      h = sym_hashes[r_symndx - symtab_hdr->sh_info];
+      while (h->root.type == bfd_link_hash_indirect
+          || h->root.type == bfd_link_hash_warning)
+        h = (struct elf_link_hash_entry *) h->root.u.i.link;
+
+      /* PR15323, ref flags aren't set for references in the same
+         object.  */
+      h->root.non_ir_ref = 1;
+    }
+  }
+
+  return TRUE;
+}
+
 #define TARGET_BIG_SYM		aqua_elf32_vec
 #define TARGET_BIG_NAME		"elf32-aqua"
 #define ELF_ARCH		bfd_arch_aqua
@@ -186,5 +408,11 @@ aqua_elf_info_to_howto (bfd *abfd ATTRIBUTE_UNUSED,
 #define bfd_elf32_bfd_reloc_type_lookup aqua_elf_reloc_type_lookup
 #define bfd_elf32_bfd_reloc_name_lookup aqua_elf_reloc_name_lookup
 #define elf_info_to_howto		            aqua_elf_info_to_howto
+#define elf_backend_relocate_section		aqua_elf_relocate_section
+#define elf_backend_gc_mark_hook		    aqua_elf_gc_mark_hook
+#define elf_backend_check_relocs        aqua_elf_check_relocs
+
+#define elf_backend_can_gc_sections	1
+#define elf_backend_rela_normal			1
 
 #include "elf32-target.h"
